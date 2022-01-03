0816e211c998dd2632974aa1da3cb1b106d2980f
Chris Chuang <chrischuang0421@gmail.com>
2022-01-03 14:36:13 +0800

    feat: Add header logo.

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import type { FC } from "react";
 import style from "./header.module.scss";
+import logoSrc from "./images/taiwan-logo.png";
 
 export interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = () => {
-  return <div className={style.container}></div>;
+  return (
+    <div className={style.container}>
+      <Link href="/">
+        <a className={style.logo}>
+          <Image src={logoSrc} width={99} height={57} alt="logo" />
+        </a>
+      </Link>
+    </div>
+  );
 };
