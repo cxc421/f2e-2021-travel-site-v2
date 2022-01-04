295ae014e5820d19cbe6fbb47e399a9c0887fcc1
Chris Chuang <chrischuang0421@gmail.com>
2022-01-04 10:23:18 +0800

    feat: Change loading font display to swap.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Roboto&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Roboto&display=swap"
             rel="stylesheet"
           />
         </Head>
