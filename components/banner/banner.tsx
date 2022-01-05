3e13f657f335891b5e4a63fb03b5effe491d6455
Chris Chuang <chrischuang0421@gmail.com>
2022-01-06 01:37:02 +0800

    feat: Add banner shadow

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -10,29 +10,32 @@ export interface Banner {
 
 export const Banner: FC<Banner> = ({ bgSrc, filterContent }) => {
   return (
-    <div className={style.container}>
-      <div className={style.bgImgWrapper}>
-        <Image
-          className={style.bgImg}
-          src={bgSrc}
-          alt="banner"
-          layout="fill"
-          objectFit="cover"
-          priority
-        />
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.bgImgWrapper}>
+          <Image
+            className={style.bgImg}
+            src={bgSrc}
+            alt="banner"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
+        <div className={style.sloganImgWrapper}>
+          <Image
+            src={sloganImgSrc}
+            alt="Welcome to Taiwan"
+            width={488}
+            height={71}
+          />
+        </div>
+        <h1 className={style.sloganText}>
+          台北、台中、台南、屏東、宜蘭……遊遍台灣
+        </h1>
+        <div className={style.filterArea}>{filterContent}</div>
       </div>
-      <div className={style.sloganImgWrapper}>
-        <Image
-          src={sloganImgSrc}
-          alt="Welcome to Taiwan"
-          width={488}
-          height={71}
-        />
-      </div>
-      <h1 className={style.sloganText}>
-        台北、台中、台南、屏東、宜蘭……遊遍台灣
-      </h1>
-      <div className={style.filterArea}>{filterContent}</div>
+      <div className={style.shadow} />
     </div>
   );
 };
