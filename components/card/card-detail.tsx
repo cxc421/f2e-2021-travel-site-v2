2ea241fcf0780ba2f18cd026d82019037989405b
Chris Chuang <chrischuang0421@gmail.com>
2022-01-12 15:02:52 +0800

    style: Refine detail card style in small screen size.

--- components/card/card-detail.tsx
+++ components/card/card-detail.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { FC } from "react";
+import cn from "classnames";
 import style from "./card-detail.module.scss";
 import gpsImgSrc from "./images/gps.png";
 
@@ -17,23 +18,35 @@ export const CardDetail: FC<CardDetailProps> = ({
   location = "",
 }) => {
   return (
-    <div className={style.container}>
-      <div className={style.imgArea}>
-        <Image src={img || ""} layout="fill" alt={title} objectFit="cover" />
-      </div>
-      <div className={style.content}>
-        <h3 className={style.title}>{title}</h3>
-        <p className={style.description}>{description}</p>
-        <div className={style.bottom}>
-          <div className={style.location}>
-            <Image src={gpsImgSrc} width={16} height={20} alt="gps" />
-            <span>{location}</span>
-          </div>
-          <div role="button" className={style.detailButton}>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.imgArea}>
+          <Image
+            className={style.img}
+            src={img || ""}
+            layout="fill"
+            alt={title}
+            objectFit="cover"
+          />
+          <div role="button" className={cn(style.detailButton, style.active)}>
             活動詳情
           </div>
         </div>
+        <div className={style.content}>
+          <h3 className={style.title}>{title}</h3>
+          <p className={style.description}>{description}</p>
+          <div className={style.bottom}>
+            <div className={style.location}>
+              <Image src={gpsImgSrc} width={16} height={20} alt="gps" />
+              <span>{location}</span>
+            </div>
+            <div role="button" className={style.detailButton}>
+              活動詳情
+            </div>
+          </div>
+        </div>
       </div>
+      <div className={style.shadow} />
     </div>
   );
 };
