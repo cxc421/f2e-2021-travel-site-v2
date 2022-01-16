5eb039f1fc8de39f35771bdba0b611bae13722a2
Chris Chuang <chrischuang0421@gmail.com>
2022-01-16 22:48:43 +0800

    chore: add test detail card data

---
+++ components/card/card-detail-test-data.ts
@@ -0,0 +1,24 @@
+import { CardDetailProps } from "./card-detail";
+export const cardDetailTestData: CardDetailProps = {
+  title: "合歡山國際暗空公園-星空清境跨年活動",
+  description:
+    "南投縣與各單位多年於合歡山舉辦清境高山跨年晚會活動，今年將活動主軸由傳統跨年晚會轉化成為台灣高山星空遊程之體驗活動，在擁有東南亞區最佳的星空觀測環境。奇特造型，值得深入觀賞體會。",
+  images: [
+    {
+      url: "https://www.taiwan.net.tw/att/event/f401957f-72ff-4c95-9a51-b59a6194bed5.jpg",
+      description: "火樹銀花",
+    },
+    {
+      url: "https://www.taiwan.net.tw/att/event/73f19944-a9df-4d32-a895-05735b7c1443.jpg",
+      description: "絢麗多彩",
+    },
+    {
+      url: "https://www.taiwan.net.tw/att/event/7dbc0a7f-cd94-482a-9ad6-bbe2c940712c.jpg",
+      description: "清境跨年演唱會",
+    },
+  ],
+  time: "開放式空間，無時間限制",
+  price: "免費",
+  address: "基隆市中山區湖海路一、二段(協和街)",
+  phone: "886-2-24287664",
+};
