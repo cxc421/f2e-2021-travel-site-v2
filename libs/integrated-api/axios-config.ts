280a0c45b5d7d7079bd22f83fa4dad1609a7c9be
Chris Chuang <chrischuang0421@gmail.com>
2022-01-24 16:51:40 +0800

    feat: update heroku server url

--- libs/integrated-api/axios-config.ts
+++ libs/integrated-api/axios-config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const isBrowser = typeof window !== "undefined";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://motc-server.herokuapp.com",
+  baseURL: "https://motc-server-220124.herokuapp.com/",
   // baseURL: "http://127.0.0.1:5000",
   headers: isBrowser ? {} : { "Accept-Encoding": "gzip" },
 });
--- libs/integrated-api/integrated-api.ts
+++ libs/integrated-api/integrated-api.ts
@@ -5,7 +5,10 @@ export const getIntegratedData = async (
   filter: IntegratedDataFilter
 ): Promise<IntegratedData[]> => {
   const response = await axiosInstance.get("/api/tourism", {
-    params: filter,
+    params: {
+      ...filter,
+      _: Date.now(),
+    },
   });
   return response.data;
 };
