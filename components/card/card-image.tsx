66ea499153cff4c278c421c6492ec00a277d7568
Chris Chuang <chrischuang0421@gmail.com>
2022-01-25 13:27:04 +0800

    fix: invalid setState after card-image unmounted

--- components/card/card-image.tsx
+++ components/card/card-image.tsx
@@ -5,16 +5,30 @@ import cn from "classnames";
 import style from "./card-image.module.scss";
 import noImgSrc from "./images/no-img.png";
 
+function useIsMounted(): boolean {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  return isMounted;
+}
+
 export const CardImage: FC<ImageProps> = ({
   src,
   alt,
   onError,
   ...otherProps
 }) => {
+  const isMounted = useIsMounted();
   const [hasError, setHasError] = useState(
     typeof src !== "string" || src.length === 0
   );
   const [loadSuccess, setLoadSuccess] = useState(false);
+
   const imageSrcRef = useRef(src);
 
   useEffect(() => {
@@ -40,18 +54,27 @@ export const CardImage: FC<ImageProps> = ({
   }
 
   return (
-    <div className={cn({ [style.successImgWrapper]: loadSuccess })}>
+    <div
+      className={cn(style.successImgWrapper, {
+        [style.isSuccess]: loadSuccess,
+      })}
+    >
       <Image
         src={src}
         alt={alt}
         {...otherProps}
         onError={(e) => {
+          if (!isMounted) return;
+
           if (onError) {
             onError(e);
           }
           setHasError(true);
         }}
-        onLoadingComplete={() => setLoadSuccess(true)}
+        onLoadingComplete={() => {
+          if (!isMounted) return;
+          setLoadSuccess(true);
+        }}
       />
     </div>
   );
