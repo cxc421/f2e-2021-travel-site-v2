2a13d9f99285fc44f7ba9e79b68cbd41bb5b479f
Chris Chuang <chrischuang0421@gmail.com>
2022-01-25 10:15:23 +0800

    refactor: remove unused react-query package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,16 @@
 import "normalize.css";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { useState } from "react";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Head>
         <title>Taiwan° Tourguide</title>
       </Head>
       <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    </>
   );
 }
 
